a8a3292b7250a9adbfa1adcaf6caf708c8841692
lokeshnaivaidya-max <lokesh.naivaidya@gmail.com>
2026-07-02 18:39:25 +0530

    add growth framework, roadmap and CTA sections

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,11 @@ import HeroSection from '../components/about/HeroSection'
 import WhoWeAre from '../components/about/WhoWeAre'
 import VisionMission from '../components/about/VisionMission'
 import PhilosophySection from '../components/about/PhilosophySection'
+import GrowthFramework from '../components/about/GrowthFramework'
 import ServicesSection from '../components/about/ServicesSection'
+import FutureRoadmap from '../components/about/FutureRoadmap'
 import ResourcesSection from '../components/about/ResourcesSection'
+import BrandClosingCTA from '../components/about/BrandClosingCTA'
 import ContactSection from '../components/about/ContactSection'
 
 export default function About() {
@@ -13,8 +16,11 @@ export default function About() {
       <WhoWeAre />
       <VisionMission />
       <PhilosophySection />
+      <GrowthFramework />
       <ServicesSection />
+      <FutureRoadmap />
       <ResourcesSection />
+      <BrandClosingCTA />
       <ContactSection />
     </>
   )
