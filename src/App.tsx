9ba61e0b1879e7c15fd7b823a5d7d83454946f45
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-03 16:30:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { Route, Switch } from 'wouter'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import About from './pages/About'
+import Services from './pages/Services'
+import Solutions from './pages/Solutions'
+import Ecosystem from './pages/Ecosystem'
+import KnowledgeHub from './pages/KnowledgeHub'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
@@ -11,6 +16,11 @@ export default function App() {
         <Switch>
           <Route path="/" component={About} />
           <Route path="/about" component={About} />
+          <Route path="/services" component={Services} />
+          <Route path="/solutions" component={Solutions} />
+          <Route path="/ecosystem" component={Ecosystem} />
+          <Route path="/knowledge" component={KnowledgeHub} />
+          <Route path="/contact" component={Contact} />
         </Switch>
       </main>
       <Footer />
