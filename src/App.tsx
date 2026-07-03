e2e5fd77380d824f808a6337e6f0159a7d749796
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-03 16:46:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import About from './pages/About'
 import Services from './pages/Services'
-import Solutions from './pages/Solutions'
+import TargetAudience from './pages/TargetAudience'
 import Ecosystem from './pages/Ecosystem'
+import Differentiator from './pages/Differentiator'
+import CoreValues from './pages/CoreValues'
 import KnowledgeHub from './pages/KnowledgeHub'
 import Contact from './pages/Contact'
 
@@ -17,8 +19,10 @@ export default function App() {
           <Route path="/" component={About} />
           <Route path="/about" component={About} />
           <Route path="/services" component={Services} />
-          <Route path="/solutions" component={Solutions} />
+          <Route path="/target-audience" component={TargetAudience} />
           <Route path="/ecosystem" component={Ecosystem} />
+          <Route path="/differentiator" component={Differentiator} />
+          <Route path="/core-values" component={CoreValues} />
           <Route path="/knowledge" component={KnowledgeHub} />
           <Route path="/contact" component={Contact} />
         </Switch>
