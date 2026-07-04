2e681958c59ca149b140869497875d1c2c3f7f40
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-04 18:10:27 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './lib/auth-context'
 import App from './App'
 import './index.css'
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
