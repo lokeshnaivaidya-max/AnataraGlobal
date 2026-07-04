297b892d94dcd7ed1be7288ef1c3962538d3ef3b
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-04 20:06:55 +0530

    Add files via upload

---
+++ src/lib/founder-hooks.ts
@@ -0,0 +1,199 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import api from './api'
+import type {
+  Founder,
+  Education,
+  Experience,
+  Skill,
+  Startup,
+  TeamMember,
+  Document,
+  BusinessStage,
+  FundingStage,
+  CapTableEntry,
+} from './founder-types'
+
+export function useFounderProfile() {
+  return useQuery({
+    queryKey: ['founder-profile'],
+    queryFn: () => api.get<Founder>('/founder/profile').then(res => res.data),
+  })
+}
+
+export function useUpdateFounderProfile() {
+  return useMutation({
+    mutationFn: (data: Partial<Founder>) =>
+      api.put<Founder>('/founder/profile', data).then(res => res.data),
+  })
+}
+
+export function useEducation() {
+  return useQuery({
+    queryKey: ['founder-education'],
+    queryFn: () => api.get<Education[]>('/founder/education').then(res => res.data),
+  })
+}
+
+export function useAddEducation() {
+  return useMutation({
+    mutationFn: (data: Omit<Education, 'id' | 'founderId'>) =>
+      api.post<Education>('/founder/education', data).then(res => res.data),
+  })
+}
+
+export function useUpdateEducation() {
+  return useMutation({
+    mutationFn: ({ id, data }: { id: string; data: Partial<Education> }) =>
+      api.put<Education>(`/founder/education/${id}`, data).then(res => res.data),
+  })
+}
+
+export function useDeleteEducation() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/founder/education/${id}`),
+  })
+}
+
+export function useExperience() {
+  return useQuery({
+    queryKey: ['founder-experience'],
+    queryFn: () => api.get<Experience[]>('/founder/experience').then(res => res.data),
+  })
+}
+
+export function useAddExperience() {
+  return useMutation({
+    mutationFn: (data: Omit<Experience, 'id' | 'founderId'>) =>
+      api.post<Experience>('/founder/experience', data).then(res => res.data),
+  })
+}
+
+export function useUpdateExperience() {
+  return useMutation({
+    mutationFn: ({ id, data }: { id: string; data: Partial<Experience> }) =>
+      api.put<Experience>(`/founder/experience/${id}`, data).then(res => res.data),
+  })
+}
+
+export function useDeleteExperience() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/founder/experience/${id}`),
+  })
+}
+
+export function useSkills() {
+  return useQuery({
+    queryKey: ['founder-skills'],
+    queryFn: () => api.get<Skill[]>('/founder/skills').then(res => res.data),
+  })
+}
+
+export function useAddSkill() {
+  return useMutation({
+    mutationFn: (data: Omit<Skill, 'id' | 'founderId'>) =>
+      api.post<Skill>('/founder/skills', data).then(res => res.data),
+  })
+}
+
+export function useDeleteSkill() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/founder/skills/${id}`),
+  })
+}
+
+export function useStartup() {
+  return useQuery({
+    queryKey: ['startup'],
+    queryFn: () => api.get<Startup>('/startup').then(res => res.data),
+  })
+}
+
+export function useUpdateStartup() {
+  return useMutation({
+    mutationFn: (data: Partial<Startup>) =>
+      api.put<Startup>('/startup', data).then(res => res.data),
+  })
+}
+
+export function useBusinessStages() {
+  return useQuery({
+    queryKey: ['business-stages'],
+    queryFn: () => api.get<BusinessStage[]>('/startup/business-stages').then(res => res.data),
+  })
+}
+
+export function useFundingStages() {
+  return useQuery({
+    queryKey: ['funding-stages'],
+    queryFn: () => api.get<FundingStage[]>('/startup/funding-stages').then(res => res.data),
+  })
+}
+
+export function useTeamMembers() {
+  return useQuery({
+    queryKey: ['team-members'],
+    queryFn: () => api.get<TeamMember[]>('/startup/team').then(res => res.data),
+  })
+}
+
+export function useAddTeamMember() {
+  return useMutation({
+    mutationFn: (data: Omit<TeamMember, 'id'>) =>
+      api.post<TeamMember>('/startup/team', data).then(res => res.data),
+  })
+}
+
+export function useUpdateTeamMember() {
+  return useMutation({
+    mutationFn: ({ id, data }: { id: string; data: Partial<TeamMember> }) =>
+      api.put<TeamMember>(`/startup/team/${id}`, data).then(res => res.data),
+  })
+}
+
+export function useDeleteTeamMember() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/startup/team/${id}`),
+  })
+}
+
+export function useDocuments() {
+  return useQuery({
+    queryKey: ['founder-documents'],
+    queryFn: () => api.get<Document[]>('/founder/documents').then(res => res.data),
+  })
+}
+
+export function useUploadDocument() {
+  return useMutation({
+    mutationFn: (formData: FormData) =>
+      api.post<Document>('/founder/documents/upload', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      }).then(res => res.data),
+  })
+}
+
+export function useDeleteDocument() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/founder/documents/${id}`),
+  })
+}
+
+export function useCapTable() {
+  return useQuery({
+    queryKey: ['cap-table'],
+    queryFn: () => api.get<CapTableEntry[]>('/startup/cap-table').then(res => res.data),
+  })
+}
+
+export function useAddCapTableEntry() {
+  return useMutation({
+    mutationFn: (data: Omit<CapTableEntry, 'id'>) =>
+      api.post<CapTableEntry>('/startup/cap-table', data).then(res => res.data),
+  })
+}
+
+export function useDeleteCapTableEntry() {
+  return useMutation({
+    mutationFn: (id: string) => api.delete(`/startup/cap-table/${id}`),
+  })
+}
