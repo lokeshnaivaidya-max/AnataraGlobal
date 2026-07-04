e66e6f545da9357e3ce7a6f1fb3c399c5718f10c
ramnathgknaivaidya <ramnathgk.naivaidya@gmail.com>
2026-07-04 19:23:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      axios: path.resolve(__dirname, 'node_modules/axios/index.js'),
+    },
+  },
 })
